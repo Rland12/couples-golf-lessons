216e98ce73685b798b4aa2bac8a82b14bb1f1281
Rashad Landrum <rland7596@gmail.com>
2017-10-01 15:36:48 -0400

    Add files via upload

---
+++ PlayerInput.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerInput : MonoBehaviour
+{
+    private Joycon j;
+
+    void Start()
+    {
+        // get the public Joycon object attached to the JoyconManager in scene
+        j = JoyconManager.Instance.j;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (j.GetButtonDown(Joycon.Button.SHOULDER_2))
+        {
+            Debug.Log("Shoulder button 2 pressed");
+
+        }
+
+        if (j.GetButtonDown(Joycon.Button.SHOULDER_1))
+        {
+            Debug.Log("Shoulder button 1 pressed");
+        }
+
+        if (j.GetButtonDown(Joycon.Button.DPAD_LEFT))
+        {
+            print("left");
+        }
+
+        if (j.GetButtonDown(Joycon.Button.DPAD_RIGHT))
+        {
+            print("Right");
+        }
+    }
+    }
---
+++ ScoreTarget.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScoreTarget : MonoBehaviour
+{
+    static public int Score = 0;
+    public Transform[] balls;
+    BoxCollider bc;
+    // Use this for initialization
+    void Start()
+    {
+        bc = GetComponent<BoxCollider>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        foreach (Transform b in balls) {
+            if (b && bc.bounds.Contains(b.position))
+            { 
+                Score++;
+                Debug.Log(Score);
+                Destroy(b.gameObject);
+            }
+        }
+
+    }
+}
